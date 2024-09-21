4fc63ab597cc8192f1356db2223d2fa835c6ddfc
Gabriel Erick <gabrielmmve13@gmail.com>
2024-09-21 18:44:27 -0300

    realizando exercicio

--- src/gabriel/estudosJava/exercicios/secaoTres/Bee1042.java
+++ src/gabriel/estudosJava/exercicios/secaoTres/Bee1042.java
@@ -1,8 +1,22 @@
 package gabriel.estudosJava.exercicios.secaoTres;
+
 import java.util.Scanner;
 
 public class Bee1042 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] numerosDesordenados = new int[3];
+        int[] numerosOrdenado = new int[3];
+
+        for (int i = 0; i < numerosDesordenados.length; i++) {
+            numerosDesordenados[i] = sc.nextInt();
+            if (numerosDesordenados[i] > numerosDesordenados[1] && numerosDesordenados[i] > numerosDesordenados[2]) {
+                numerosOrdenado[i] = numerosDesordenados[i];
+            } else if (numerosDesordenados[i] > numerosDesordenados[1] && numerosDesordenados[i] < numerosDesordenados[2]) {
+                numerosOrdenado[i] = numerosDesordenados[i];
+            }
+        }
+        System.out.println(numerosOrdenado[0]);
+        System.out.println(numerosOrdenado[1]);
     }
 }
